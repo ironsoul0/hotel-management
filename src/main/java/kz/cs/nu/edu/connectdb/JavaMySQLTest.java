ee7c68a0b20ab856977e61397a875e24de01ed26
Sherkhan <sherkhanazim@gmail.com>
2020-10-13 18:46:23 +0600

    add in code to insert into table

--- src/main/java/kz/cs/nu/edu/connectdb/JavaMySQLTest.java
+++ src/main/java/kz/cs/nu/edu/connectdb/JavaMySQLTest.java
@@ -13,18 +13,30 @@ public class JavaMySQLTest {
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the database");
 
-            String sql = "SELECT * FROM guest";
+            // This code to add into database
+            String addsql = "INSERT INTO guest (idguest, firstname, lastname) VALUES (?, ?, ?)";
+
+            PreparedStatement addstatement = connection.prepareStatement(addsql);
+            addstatement.setInt(1, 100);
+            addstatement.setString(2, "Tima");
+            addstatement.setString(3, "Yuss");
+
+            int rows = addstatement.executeUpdate();
+            System.out.println(rows);
+
+            // This code to iterate through all guests
+            String  sql = "SELECT * FROM guest";
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery(sql);
-
             int count = 0;
-
             while(result.next()) {
                 String firstname = result.getString("firstname");
                 String lastname = result.getString("lastname");
                 count++;
-                System.out.println("Guest:" + firstname + " " + lastname);
+                System.out.println("Guest: " + firstname + " " + lastname);
             }
+            statement.close();
+            connection.close();
         } catch (SQLException e) {
             System.out.println("Oops, error!");
             e.printStackTrace();
