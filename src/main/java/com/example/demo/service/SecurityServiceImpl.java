387deeb655694871af78f7381120c1a968ca04a6
ironsoul0 <timka2609@gmail.com>
2020-10-27 14:45:50 +0600

    qualifier

--- src/main/java/com/example/demo/service/SecurityServiceImpl.java
+++ src/main/java/com/example/demo/service/SecurityServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +16,7 @@ public class SecurityServiceImpl implements SecurityService{
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Qualifier("guestDetailsServiceImpl")
     @Autowired
     private UserDetailsService userDetailsService;
 
