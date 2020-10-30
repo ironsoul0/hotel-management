7deccdc735179696a482d709e2177a120088bbc3
ironsoul0 <timka2609@gmail.com>
2020-10-30 13:23:42 +0600

    structure react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { Counter } from "./store/reducers/Counter";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
