ff3d76511a967a2138bd996df2194f876a73e8a6
ironsoul0 <timka2609@gmail.com>
2020-11-11 15:07:27 +0600

    rest

--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -36,7 +36,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-//@Controller
+// @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/profile")
