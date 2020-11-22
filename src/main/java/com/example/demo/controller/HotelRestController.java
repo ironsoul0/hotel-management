18fd01bf1d17f2d7120e4688c73f4b8352c016df
Sherkhan <sherkhanazim@gmail.com>
2020-11-22 14:05:45 +0600

    the season's price added to the base price

--- src/main/java/com/example/demo/controller/HotelRestController.java
+++ src/main/java/com/example/demo/controller/HotelRestController.java
@@ -1,13 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Hotel;
-import com.example.demo.model.Reservation;
-import com.example.demo.model.Room_type;
-import com.example.demo.model.User;
-import com.example.demo.repository.HotelRepository;
-import com.example.demo.repository.ReservationRepository;
-import com.example.demo.repository.Room_typeRepository;
-import com.example.demo.repository.UserRepository;
+import com.example.demo.model.*;
+import com.example.demo.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -24,8 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -43,6 +37,9 @@ public class HotelRestController {
 
     @Autowired
     private ReservationRepository reservationRepository;
+
+    @Autowired
+    private TakesPlaceInRepository takesPlaceInRepository;
 
     private String getUsername(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -67,14 +64,41 @@ public class HotelRestController {
         return diff * bp;
     }
 
+    private int addSeasonPrice(int basePrice, Date d1, Date d2, Hotel hotel) throws ParseException {
+        List<TakesPlaceIn> allTakes =  takesPlaceInRepository.findAll();
+        int totalprice = basePrice;
+        for (TakesPlaceIn takes : allTakes) {
+            if (takes.getHotel() == hotel) {
+                List<String> prices = Arrays.asList(takes.getWeekdayPrice().split(","));
+                Calendar start = Calendar.getInstance();
+                start.setTime(d1);
+                Calendar end = Calendar.getInstance();
+                end.setTime(d2);
+
+                Date startSeason = new SimpleDateFormat("yyyy-MM-dd").parse(takes.getSeason().getStartDate());
+                Date endSeason = new SimpleDateFormat("yyyy-MM-dd").parse(takes.getSeason().getEndDate());
+                for (Date date = start.getTime(); start.before(end); start.add(Calendar.DATE, 1), date = start.getTime()) {
+                    if (startSeason.getTime() <= date.getTime() && date.getTime() <= endSeason.getTime()) {
+                        Calendar c = Calendar.getInstance();
+                        c.setTime(date);
+                        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+                        totalprice += Integer.parseInt(prices.get(dayOfWeek - 1));
+                    }
+                }
+            }
+        }
+        return totalprice;
+    }
+
     @PostMapping("/book-room")
     public String bookRoomReal(@RequestParam Long roomtype, @RequestParam String myDate, @RequestParam String myDate2, Model model) throws ParseException {
         Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(myDate);
         Date date2 = new SimpleDateFormat("yyyy-MM-dd").parse(myDate2);
         User u = userRepository.findByUsername(getUsername()).get();
         Room_type rt = room_typeRepository.findById(roomtype).get();
-        long price = calculatePrice(date1, date2, rt.getBase_price());
-        Reservation r = new Reservation(date1, date2, (int) price, 1, u, false);
+        int price = (int) calculatePrice(date1, date2, rt.getBase_price());
+        int finalPrice = addSeasonPrice(price, date1, date2, rt.getHotel_id());
+        Reservation r = new Reservation(date1, date2, finalPrice, 1, u, false);
         r.setRoom_type_id(rt);
         reservationRepository.save(r);
         return "Success";
@@ -85,8 +109,9 @@ public class HotelRestController {
         Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse(myDate);
         Date date2 = new SimpleDateFormat("yyyy-MM-dd").parse(myDate2);
         Room_type rt = room_typeRepository.findById(roomtype).get();
-        long price = calculatePrice(date1, date2, rt.getBase_price());
-        return price;
+        long price =  calculatePrice(date1, date2, rt.getBase_price());
+        int finalPrice = addSeasonPrice( (int) price, date1, date2, rt.getHotel_id());
+        return (long) finalPrice;
     }
 
     @GetMapping("/")
